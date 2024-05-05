abdbcd089d6cc060e035a879745b079340334135
Neri Marschik <codesuki@users.noreply.github.com>
2024-05-05 14:49:51 +0900

    fix: previous commit broke kernel
    I think I lost some code. Maybe uncomitted on an old computer.
    This reverts back to 32bit mode which solves the triple fault during boot.

--- Makefile
+++ Makefile
@@ -8,13 +8,13 @@ C_SOURCES = $(wildcard *.c)
 OBJECTS += $(C_SOURCES:.c=.o)
 
 LD = x86_64-elf-ld
-LDFLAGS = -T linker.ld -nostdlib -z max-page-size=0x1000
+LDFLAGS = -T linker.ld -nostdlib -z max-page-size=0x1000 -melf_i386
 
 CC = x86_64-elf-gcc
-CFLAGS = -g -Wall -Wextra -ffreestanding -mno-red-zone
+CFLAGS = -g -Wall -Wextra -ffreestanding -mno-red-zone -m32
 
 AS = nasm
-ASFLAGS = -f elf64 -g -F dwarf
+ASFLAGS = -f elf32 -g -F dwarf
 
 .PHONY: all clean run debug
 
@@ -25,7 +25,13 @@ clean:
 	@rm -r dist/
 
 run:
-	qemu-system-x86_64 -d int -no-reboot -cdrom $(ISO) -s
+#-d int is
+# show interrupts/exceptions in short format
+# -s is
+# shorthand for -gdb tcp::1234
+# -S is
+# freeze CPU at startup (use 'c' to start execution)
+	qemu-system-x86_64 -d int -no-reboot -cdrom $(ISO) -s -no-shutdown
 
 debug:
 	qemu-system-x86_64 -d int -no-reboot -cdrom $(ISO) -s -S
--- loader.s
+++ loader.s
@@ -9,8 +9,10 @@ section .text
 global loader
 loader:
         call check_multiboot
-        ; set up the stack
+
+        ; set up the stack. Could use label after stack instead of addition
         mov esp, stack+STACKSIZE
+
         push eax                           ; pass Multiboot magic number
         push ebx                           ; pass Multiboot info structure
 
@@ -22,19 +24,19 @@ hang:
         jmp   hang
 
 handle_error:
-        mov dword [0xb8000], 0x04410450 ; prints PANIC in red letters 0x04 <- red, 0x41 <- A
+        mov dword [0xb8000], 0x04410450 ; print PANIC in red letters 0x04 <- red, 0x41 <- A
         mov dword [0xb8004], 0x0449044E
         mov dword [0xb8008], 0x04200443
         mov byte [0xb800D], 0x04
-        mov byte [0xb800C], al
+        mov byte [0xb800C], al	; print error code
         jmp hang
 
 check_multiboot:
-        cmp eax, 0x36d76288     ;9
+        cmp eax, 0x36d76289
         jne .error
         ret
 .error:
-        mov al, 48
+        mov al, 48		; 48 = ascii 0
         jmp handle_error
 
 section .bss
